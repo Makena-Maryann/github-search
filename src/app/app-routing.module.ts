62f69d93901dc4c4090e8c532070c5d546892d4c
Makena-Maryann <maryann.makena00@gmail.com>
2020-06-21 17:42:00 +0300

    Delete home component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { UserComponent } from './user/user.component';
 
-const routes: Routes = [{ path: 'user', component: UserComponent }];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
