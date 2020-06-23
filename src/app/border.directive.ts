076ced22cce462e471a980667411dd893a601a13
Makena-Maryann <maryann.makena00@gmail.com>
2020-06-23 16:16:29 +0300

    Create directive

--- src/app/border.directive.ts
+++ src/app/border.directive.ts
@@ -1,10 +1,20 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef, HostListener } from '@angular/core';
 
 @Directive({
-  selector: '[appBorder]'
+  selector: '[appBorder]',
 })
 export class BorderDirective {
+  constructor(private el: ElementRef) {}
 
-  constructor() { }
+  @HostListener('mouseenter') onMouseEnter() {
+    this.border('2px solid grey');
+  }
 
+  @HostListener('mouseleave') onMouseLeave() {
+    this.border(null);
+  }
+
+  private border(action: string) {
+    this.el.nativeElement.style.border = action;
+  }
 }
