8c73af88f2fe1dca8bd8d5faa403e180e9ebacc9
Makena-Maryann <maryann.makena00@gmail.com>
2020-06-23 15:22:54 +0300

    Create 404 page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { UserComponent } from './user/user.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
-const routes: Routes = [{ path: 'user', component: UserComponent }];
+const routes: Routes = [
+  { path: 'user', component: UserComponent },
+  { path: '', component: UserComponent },
+  { path: '**', component: NotFoundComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
